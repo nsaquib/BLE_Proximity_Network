8aca040eb0696ea605f56a2c933341e87395959c
Dwyane George <Dwyane@dhcp-18-189-9-210.dyn.MIT.EDU>
2015-12-02 16:46:57 -0500

    wrote script for decompressing data and decompressed data collected to date.

---
+++ v03/Firmware/Decompress Memory Data Old Format.py
@@ -0,0 +1,31 @@
+import csv
+import math
+import os
+
+def decompressFile(readFile, writeFile):
+    with open(readFile, "r", errors = 'ignore') as readF:
+        with open(writeFile, "a") as writeF:
+            for line in readF:
+                writer = csv.writer(writeF)
+                value = line.rstrip()
+                values = value.split(',')
+                for value in values:
+                    if value != '' and value.isdigit():
+                        value = int(value)
+                        if value > 1000:
+                            deviceID = math.floor(value / 100000000)
+                            value -= deviceID * 100000000
+                            rssi = math.floor(value / 1000000)
+                            value -= rssi * 1000000
+                            time = value
+                            row = [time, deviceID, rssi]
+                            writer.writerow(row)
+
+for readFile in os.listdir(os.getcwd()):
+    if readFile.endswith(".txt"):
+      writeFile = readFile.split('.')[0] + ".csv"
+      with open(writeFile, "w") as file:
+          writer = csv.writer(file)
+          row = ["Timestamp", "Device ID", "RSSI"]
+          writer.writerow(row)
+      decompressFile(readFile, writeFile)
