13814682585e352bb184d4efa1ef94c07925abf9
Dwyane George <Dwyane@dhcp-18-189-26-7.dyn.MIT.EDU>
2016-01-21 16:30:56 -0500

    moved back to serial programmer for debugging firmware, works accurately

--- v03/Data Analysis/Serial Programmer.py
+++ v03/Data Analysis/Serial Programmer.py
@@ -2,22 +2,35 @@ import serial
 import time
 import datetime
 
-connected = False;
-ser = serial.Serial("/dev/cu.usbserial-DN0073UM", 9600)
-#ser = serial.Serial("/dev/cu.usbserial-DN00B462", 9600)
-#ser = serial.Serial("/dev/cu.usbserial-DN00B1WO", 9600)
+baudRate = 9600
+connected = False
+#ser = serial.Serial("/dev/cu.usbserial-DN0073UM", baudRate)
+ser = serial.Serial("/dev/cu.usbserial-DN00B462", baudRate)
+#ser = serial.Serial("/dev/cu.usbserial-DN00B1WO", baudRate)
 while not connected:
     serIn = ser.read()
     connected = True
 
 now = datetime.datetime.now()
+month = now.month
+date = now.day
+year = now.year - 2000
+day = (now.weekday() + 1) % 7
 hour = now.hour
 minute = now.minute
 sec= now.second
 ms = int(now.microsecond/1000)
 
-print(str(hour) + ":" + str(minute) + ":" + str(sec) + ":" + str(ms))
+print(str(month) + "/" + str(date) + "/" + str(year) + " " + str(day) + " " + str(hour) + ":" + str(minute) + ":" + str(sec) + ":" + str(ms))
 
+ser.write(bytes(str(month), 'ascii'))
+ser.write(bytes(',', 'ascii'))
+ser.write(bytes(str(date), 'ascii'))
+ser.write(bytes(',', 'ascii'))
+ser.write(bytes(str(year), 'ascii'))
+ser.write(bytes(',', 'ascii'))
+ser.write(bytes(str(day), 'ascii'))
+ser.write(bytes(',', 'ascii'))
 ser.write(bytes(str(hour), 'ascii'))
 ser.write(bytes(',', 'ascii'))
 ser.write(bytes(str(minute), 'ascii'))
@@ -25,4 +38,6 @@ ser.write(bytes(',', 'ascii'))
 ser.write(bytes(str(sec), 'ascii'))
 ser.write(bytes(',', 'ascii'))
 ser.write(bytes(str(ms), 'ascii'))
+
 print("Wrote bits")
+
